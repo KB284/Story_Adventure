2b2f4ebfb7cb7a387b8b7a9d65e5a668a72feaab
KB284 <khubaibfarah@gmail.com>
2023-11-22 15:28:02 -0500

    Story side_start

---
+++ Chapter1_intro.py
@@ -0,0 +1,6 @@
+def chapter_1():
+    txtfile = open(r"Chapter1 introduction.txt", "r")
+    print(txtfile.read())
+
+
+chapter_1()
---
+++ Chapter_Executer.py
@@ -0,0 +1,8 @@
+import Chapter1_intro
+
+
+# main function will hold all the chapters and execute them
+def main():
+    Chapter1_intro
+
+main()
